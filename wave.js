dbc09711e4f56af896f667600457202d69043d5d
Erik Šiffel <Erik.Siffel@KornFerry.com>
2019-11-26 21:36:56 +0100

    8

--- wave.js
+++ wave.js
@@ -5,18 +5,22 @@ class waveTest {
     for (var i in Config.Wave.Codes) {
       ConfirmitClass.lg.LogDebug("iiiicko " + i);
       ConfirmitClass.lg.LogDebug("iiiicko " +Config.Wave.Codes[i]);
-      if (Config.Wave[i] != null) {
+      if (Config.Wave.Codes[i] != "") {
         wave.push(i)
       }
     }
+      var X = [], Y = [];
+        X.push ('Methodology'{totals:false; title:true}^[N]');
 
     for (var i = 0; i < wave.length; i++) {
       ConfirmitClass.lg.LogDebug("to je vonooo " + wave[i]);
-      //Y.push ('[SEGMENT]{label:"'+ wave[i] +'"; expression:' + report.TableUtils.EncodeJsString('methodology = "'+wave[i]+ '"') + '}');
+      Y.push ('[SEGMENT]{label:"'+ wave[i] +'"; expression:' + report.TableUtils.EncodeJsString('methodology = "'+wave[i]+ '"') + '}');
     }
 
+      var expr = [y, x].join('^');
+      table.AddHeaders(report, 'ds0', expr);
 /*
-        var X = [], Y = [];
+
 
         X.push ('Methodology'{totals:false; title:true}^[N]');
         X.push ('/('+ Xsub.join('') +')')
