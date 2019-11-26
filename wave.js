3a27dcab1d127c870f130f596104e1372934b401
Erik Šiffel <Erik.Siffel@KornFerry.com>
2019-11-26 17:11:10 +0100

    log debug test 1

--- wave.js
+++ wave.js
@@ -1,3 +1,14 @@
-var wave;
-console.log("test");
-console.log("test2");
+class waveTest{
+
+function testWaves(){
+var waveCurrent = Config.Wave.Codes.Current;
+var waveTrend1 = Config.Wave.Codes.Previous;
+var waveTrend2 = Config.Wave.Codes.Previous2;
+var waveTrend3 = Config.Wave.Codes.Previous3;
+
+ConfirmitClass.lg.LogDebug(waveCurrent);
+ConfirmitClass.lg.LogDebug(waveTrend1);
+ConfirmitClass.lg.LogDebug(waveTrend2);
+ConfirmitClass.lg.LogDebug(waveTrend3);
+}
+}
