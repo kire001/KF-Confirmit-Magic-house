0e2c367e9ad85739633e491a78cb779c74848b25
Erik Šiffel <Erik.Siffel@KornFerry.com>
2019-11-27 14:41:19 +0100

    38

--- wave.js
+++ wave.js
@@ -42,7 +42,7 @@ class waveTest {
     ConfirmitClass.lg.LogDebug("1111111111111");
     var values = report.TableUtils.GetColumnValues('rr_overall:ResponseRate',1);
     ConfirmitClass.lg.LogDebug("22222222222222");
-    var datapoints = report.TableUtils.GetColumnValues("myTable", 0);
+    var datapoints = report.TableUtils.GetColumnValues('tester:myTable', 0);
     ConfirmitClass.lg.LogDebug("3333333333333333");
     //report.TableUtils.GetColumnValues('ResponseRate',1);
     //  var k;
